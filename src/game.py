dabc5aaf89ebc4da94e078f265837710a5f47804
Matteo Bongiovanni <mgb58@hotmail.co.uk>
2021-10-20 19:00:59 +0100

    Add a function that prints the results

--- src/game.py
+++ src/game.py
@@ -4,3 +4,14 @@
     """
 
     return symbols
+
+
+def print_symbols_as_moves(symbols):
+    """
+    Symbols msut be a tuple of three elements
+    """
+    index_in_symbols = {"rock": 0, "paper": 1, "scissors": 2}
+
+    print(f"{symbols[index_in_symbols['rock']]} - Rock")
+    print(f"{symbols[index_in_symbols['paper']]} - Paper")
+    print(f"{symbols[index_in_symbols['scissors']]} - Scissors")
